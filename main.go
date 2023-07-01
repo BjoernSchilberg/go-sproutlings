185fceb7526beb040a7ce0bb31cd8090c5b7f830
BjoernSchilberg <bjoern.schilberg@posteo.de>
2023-07-01 22:26:47 +0200

    Added first single character

--- main.go
+++ main.go
@@ -13,11 +13,14 @@ var (
 	bkgColor     = rl.NewColor(147, 211, 196, 255)
 	grassSprite  rl.Texture2D
 	playerSprite rl.Texture2D
+
+	playerSrc  rl.Rectangle
+	playerDest rl.Rectangle
 )
 
 func drawScence() {
 	rl.DrawTexture(grassSprite, 100, 50, rl.White)
-	rl.DrawTexture(playerSprite, 200, 200, rl.White)
+	rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0, rl.White)
 
 }
 func input() {}
@@ -45,6 +48,8 @@ func init() {
 
 	grassSprite = rl.LoadTexture("res/Sprout Lands - Sprites - Basic pack/Tilesets/ground tiles/old tiles/Grass.png")
 	playerSprite = rl.LoadTexture("res/Sprout Lands - Sprites - Basic pack/Characters/Basic Charakter Spritesheet.png")
+	playerSrc = rl.NewRectangle(0, 0, 48, 48)
+	playerDest = rl.NewRectangle(200, 200, 100, 100)
 }
 
 func quit() {
