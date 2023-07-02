6c2ec2cab3e5538c42f76e78f5fef4d1b4a7adb0
BjoernSchilberg <bjoern.schilberg@posteo.de>
2023-07-02 22:42:39 +0200

    Basic tile mapping handling

--- main.go
+++ main.go
@@ -9,9 +9,10 @@ const (
 )
 
 var (
-	running      = true
-	bkgColor     = rl.NewColor(147, 211, 196, 255)
-	grassSprite  rl.Texture2D
+	running     = true
+	bkgColor    = rl.NewColor(147, 211, 196, 255)
+	grassSprite rl.Texture2D
+
 	playerSprite rl.Texture2D
 
 	playerSrc    rl.Rectangle
@@ -24,6 +25,12 @@ var (
 
 	frameCount int
 
+	tileDest   rl.Rectangle
+	tileSrc    rl.Rectangle
+	tileMap    []int
+	srcMap     []string
+	mapW, mapH int
+
 	playerSpeed float32 = 3
 
 	musicPaused bool
@@ -33,7 +40,19 @@ var (
 )
 
 func drawScence() {
-	rl.DrawTexture(grassSprite, 100, 50, rl.White)
+	//rl.DrawTexture(grassSprite, 100, 50, rl.White)
+	//
+	for i := 0; i < len(tileMap); i++ {
+		// Make sure tileMap is not empty
+		if tileMap[i] != 0 {
+			tileDest.X = tileDest.Width * float32(i%mapW)
+			tileDest.Y = tileDest.Height * float32(i/mapW)
+			tileSrc.X = tileSrc.Width * float32((tileMap[i]-1)%int(grassSprite.Width/int32(tileSrc.Width)))
+			tileSrc.Y = tileSrc.Height * float32((tileMap[i]-1)/int(grassSprite.Width/int32(tileSrc.Width)))
+
+			rl.DrawTexturePro(grassSprite, tileSrc, tileDest, rl.NewVector2(tileDest.Width, tileDest.Height), 0, rl.White)
+		}
+	}
 	rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0, rl.White)
 
 }
@@ -129,6 +148,16 @@ func render() {
 	rl.EndDrawing()
 }
 
+func loadMap() {
+
+	mapW = 5
+	mapH = 5
+	for i := 0; i < (mapW * mapH); i++ {
+		tileMap = append(tileMap, 1)
+	}
+
+}
+
 func init() {
 
 	rl.InitWindow(screenWidth, screenHeight, "Sproutlings")
@@ -139,6 +168,10 @@ func init() {
 	rl.SetTargetFPS(60)
 
 	grassSprite = rl.LoadTexture("res/Sprout Lands - Sprites - Basic pack/Tilesets/ground tiles/old tiles/Grass.png")
+
+	tileDest = rl.NewRectangle(0, 0, 16, 16)
+	tileSrc = rl.NewRectangle(0, 0, 16, 16)
+
 	playerSprite = rl.LoadTexture("res/Sprout Lands - Sprites - Basic pack/Characters/Basic Charakter Spritesheet.png")
 	playerSrc = rl.NewRectangle(0, 0, 48, 48)
 	playerDest = rl.NewRectangle(200, 200, 100, 100)
@@ -151,6 +184,8 @@ func init() {
 	// Note: Character stays in the center of the screen.
 	cam = rl.NewCamera2D(rl.NewVector2(float32(screenWidth/2), float32(screenHeight/2)), rl.NewVector2(float32(playerDest.X-(playerDest.Width/2)), float32(playerDest.Y-(playerDest.Height/2))), 0.0, 1.0)
 	cam.Zoom = 2.0
+
+	loadMap()
 
 }
 
